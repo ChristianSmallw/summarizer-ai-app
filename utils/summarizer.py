a3e592fd9863eb3ee91fce18a9a0186899bd197a
ChristianSmallw <christian.smallw@gmail.com>
2025-10-02 22:47:25 -0400

    Fixed a bug where some text would give an error because its not in UTF-8.

--- utils/summarizer.py
+++ utils/summarizer.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 from utils.config import get_secret
 import re
 import time
+from ftfy import fix_text
 
 OPENAI_API_KEY = get_secret("OPENAI_API_KEY")
 # OLLAMA_API = "http://localhost:11434/api/chat"
@@ -45,23 +46,9 @@ def strip_thinking(text: str) -> str:
 
 
 def summarize_text(text, model_name, is_local, prompt="Summarize this:"):
-    # response = client.chat.completions.create(
-    #     model=model_name,
-    #     #model="qwen3:32b",
-    #     messages=[
-    #         #{"role": "system", "content": "You are a helpful but very sassy and aloof cat that summarizes articles in a feline way. Use cat puns, end some lines with 'meow~', and throw in a random nap reference if possible."},
-    #         {"role": "system", "content": "You are an assistant that analyzes text and provides a summary, ignoring text that might be navigation related."},
-    #         {"role": "user", "content": f"{prompt}\n\n{text}"}
-    #     ],
-        
-    #     temperature=0.8
-    #     # max_tokens=16000
-    # )
-    # # raw = response.choices[0].message.content
-    # # clean = strip_thinking(raw)
-    # # return clean
-    # return response.choices[0].message.content
     temp = 1 if model_name.startswith("gpt-5") else 0.2
+    text = fix_text(text)
+
     if not is_local:
         response = client.responses.create(
             model=model_name,
@@ -72,14 +59,6 @@ def summarize_text(text, model_name, is_local, prompt="Summarize this:"):
         )
         return response.output_text
     else:
-        # response = ollama_client.chat.completions.create(
-        #     model=model_name,
-        #     instructions="You are an assistant that analyzes text and provides a summary, ignoring text that might be navigation related.",
-        #     input=f"{prompt}\n\n{text}",
-        #     temperature=temp
-        # )
-        # raw = response.choices[0].message.content
-        # clean = strip_thinking(raw)
         r = requests.post(
             "https://reese-shingly-johnetta.ngrok-free.dev/summarize",
             json={
